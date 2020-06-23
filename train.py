9a4baf9530c7f845066cb836899e75215dea1b9b
rochefca <camille.rochefortboulanger@gmail.com>
2020-06-22 22:58:48 -0400

    Training loop

--- train.py
+++ train.py
@@ -1,10 +1,16 @@
 import argparse
 import os
+import time
 
 import numpy as np
+
 import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+import torch.nn.functional as F
 
 import dataset_utils as du
+import model as model
 
 
 def main():
@@ -54,20 +60,24 @@ def main():
     x_valid_normed = du.normalize(x_valid, mus, sigmas)
     x_test_normed = du.normalize(x_test, mus, sigmas)
 
-    # Make fold final dataset (xs and ys are casted to tensors)
+    # Make fold final dataset
+    """
     fold_dataset = du.FoldDataset(
             torch.cat((x_train_normed, x_valid_normed, x_test_normed), dim=0),
             torch.cat((y_train, y_valid, y_test), dim=0),
             np.concatenate((samples_train, samples_valid, samples_test))
             )
+    print(fold_dataset.xs.type())
+    """
+    train_set = du.FoldDataset(x_train_normed, y_train, samples_train)
+    valid_set = du.FoldDataset(x_valid_normed, y_valid, samples_valid)
+    test_set = du.FoldDataset(x_test_normed, y_test, samples_test)
 
-    print(fold_dataset.ys)
     # Load embedding
     emb = du.load_embedding(args.embedding, args.which_fold)
     emb = emb.to(device)
+    emb = emb.float()
 
-    print(fold_dataset.xs)
-    print(fold_dataset.ys)
     # Instantiate model
     # Input size
     n_feats_emb = emb.size()[1] # input of aux net
@@ -76,7 +86,75 @@ def main():
     emb_n_hidden_u = 100
     discrim_n_hidden1_u = 100
     discrim_n_hidden2_u = 100
-    n_targets = torch.max(fold_dataset.ys).item()
+    # Output layer
+    n_targets = max(torch.max(train_set.ys).item(),
+                    torch.max(valid_set.ys).item(),
+                    torch.max(test_set.ys).item()) + 1 #0-based encoding
+    print('\n***Nb features in models***')
+    print('n_feats_emb:', n_feats_emb)
+    print('n_feats:', n_feats)
+    # Feat embedding network
+    feat_emb_model = model.Feat_emb_net(
+            n_feats=n_feats_emb,
+            n_hidden_u=emb_n_hidden_u
+            )
+    feat_emb_model.to(device)
+    feat_emb_model_out = feat_emb_model(emb)
+    print('Feat emb. net output size:', emb.size())
+    # Discrim network
+    discrim_model = model.Discrim_net(
+            fatLayer_weights = torch.transpose(feat_emb_model_out,1,0),
+            n_feats = n_feats,
+            n_hidden1_u = discrim_n_hidden1_u,
+            n_hidden2_u = discrim_n_hidden2_u,
+            n_targets = n_targets
+            )
+    discrim_model.to(device)
+    discrim_model_out = discrim_model(train_set.xs)
+    print(discrim_model_out.size())
+
+    # Loss
+    criterion = nn.CrossEntropyLoss()
+    # Optimizer
+    lr = 0.00003
+    params = list(discrim_model.parameters()) + \
+             list(feat_emb_model.parameters())
+    optimizer = torch.optim.Adam(params, lr=lr)
+
+    # Training loop
+    n_epochs = 10
+    batch_size = 138
+    train_generator = DataLoader(train_set, batch_size=batch_size)
+    for epoch in range(n_epochs):
+        print('Epoch {} of {}'.format(epoch+1, n_epochs))
+        start_time = time.time()
+
+        feat_emb_model.train()
+        discrim_model.train()
+        for x_batch, y_batch, _ in train_generator:
+            # Make a training step
+            optimizer.zero_grad()
+            # Forward pass in aux net
+            feat_emb_model_out = feat_emb_model(emb)
+            # Forward pass in discrim net
+            fatLayer_weights = torch.transpose(feat_emb_model_out,1,0)
+            discrim_model.hidden_1.weight.data = fatLayer_weights
+            discrim_model_out = discrim_model(x_batch)
+            # Get prediction
+            yhat = F.softmax(discrim_model_out, dim=1)
+            _, pred = torch.max(yhat, dim=1)
+
+            # Compute loss
+            loss = criterion(discrim_model_out, y_batch)
+            # Compute gradients in discrim net
+            loss.backward()
+            # Copy weights of discrim net fatLayer to the output of aux net
+            fatLayer_weights.grad = discrim_model.hidden_1.weight.grad
+            # Compute gradients in feat. emb net
+            torch.autograd.backward(fatLayer_weights, fatLayer_weights.grad)
+
+            # Optim
+            optimizer.step()
 
 
 
