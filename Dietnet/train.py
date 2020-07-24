f8482ffa589672b964988dc2b074096976a96d6b
rochefca <camille.rochefortboulanger@gmail.com>
2020-07-24 14:20:13 -0400

    Saving model architecture, loss, optimizer

--- Dietnet/train.py
+++ Dietnet/train.py
@@ -139,6 +139,9 @@ def main():
                                 batch_size=batch_size,
                                 shuffle=False)
 
+    # Save model summary
+    lu.save_model_summary(out_dir, comb_model, criterion, optimizer)
+
     # Monitoring: Epoch loss and accuracy
     train_losses = []
     train_acc = []
